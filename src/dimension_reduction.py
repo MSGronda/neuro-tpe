d77b17971b92db9cc7931190bd07ab4b5caa0ae9
MSGronda <mgronda@itba.edu.ar>
2024-05-21 15:23:07 -0300

    feat: applying pca to dataset

--- src/dimension_reduction.py
+++ src/dimension_reduction.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.decomposition import PCA
 
 
 def convert_to_matrix(dataset: [{}]):
@@ -10,3 +11,24 @@ def convert_to_matrix(dataset: [{}]):
         resp.append(np.array(matrix))
 
     return resp
+
+
+def apply_pca(dataset: [np.array], n_components=None):
+    transformed = []
+    explained_variance_ratios = []
+
+    for matrix in dataset:
+
+        # TODO CHECK: sobre que hacemos la reduccion, los canales o las potencias relativas ?
+
+        if n_components is None:
+            pca = PCA(n_components=len(matrix[0]))
+        else:
+            pca = PCA(n_components)
+
+        pca.fit(matrix)
+
+        transformed.append(pca.transform(matrix))
+        explained_variance_ratios.append(pca.explained_variance_ratio_)
+
+    return transformed, explained_variance_ratios
--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from dimension_reduction import *
 if __name__ == '__main__':
     sampling_rate = 128     # En Hz
     segment_length = 150    # Ventana que se usa en el welch
+    n_components = 3        # Cantidad de components para PCA
 
     # Obtenemos el dataset y lo procesamos (aplicando FFT)
     processed_dataset = load_and_process_data(sampling_rate, segment_length)
@@ -16,11 +17,7 @@ if __name__ == '__main__':
 
     matrices = convert_to_matrix(processed_dataset)
 
+    transformed, _ = apply_pca(matrices, n_components)
 
-
-
-
-
-
-
+    print(transformed)
 
