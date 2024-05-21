2e77930cd56349131d573710b137e6807f440621
MSGronda <mgronda@itba.edu.ar>
2024-05-21 17:11:36 -0300

    feat: neural network training

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+from sklearn.model_selection import train_test_split
+
 from util import *
 from signal_processing import *
 from graphs import *
 from dimension_reduction import *
-
+from neural_network import *
 
 if __name__ == '__main__':
     sampling_rate = 128     # En Hz
@@ -15,9 +17,14 @@ if __name__ == '__main__':
     # Opcional: hacer unos heatmaps que quedan lindos
     # heatmap(processed_dataset[0], FREQUENCY_BANDS_INDEX['Sigma'])
 
+    # Aplicamos PCA
     matrices = convert_to_matrix(processed_dataset)
-
     transformed, _ = apply_pca(matrices, n_components)
 
-    print(transformed)
+    # Entrenamos la red
+    X_train, X_test, y_train, y_test = train_test_split(transformed, generate_classes(transformed), test_size=0.2, random_state=42)
+    model = train_model(X_train, y_train, 32, 10)
+
+    test_loss, test_acc = test_model(X_test, y_test, model)
+    print('Test accuracy:', test_acc)
 
---
+++ src/neural_network.py
@@ -0,0 +1,41 @@
+import numpy as np
+from keras import Sequential, layers
+
+
+def convert_to_array(dataset: [np.array]):
+    return np.array([data.flatten() for data in dataset]).astype("float32")
+
+
+def generate_classes(dataset: []):
+    return [i % 4 for i in range(len(dataset))]
+
+
+def train_model(X_train, y_train, batch_size: int, epochs: int):
+
+    X = convert_to_array(X_train)
+    y = np.array(y_train).astype("float32")
+
+    model = Sequential([
+        layers.Input(shape=X[0].shape),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(4, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy']
+    )
+
+    model.fit(X, y, batch_size=batch_size, epochs=epochs)
+
+    return model
+
+
+def test_model(X_test, y_test, model):
+    X = convert_to_array(X_test)
+    y = np.array(y_test).astype("float32")
+
+    return model.evaluate(X, y)
+
+
