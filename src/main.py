458e2b44e49a7583b88418e977b5f97db783ca1d
Marcos Gronda <mgronda@itba.edu.ar>
2024-05-29 16:18:07 -0300

    feat: added rnn and fixed multi layer

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
 from signal_processing import *
 from dimension_reduction import *
 from neural_network import *
 
 if __name__ == '__main__':
-    clip_length = 30        # En s. Particionamos el dataset en secciones para tener mas datos.
+    clip_length = 15        # En s. Particionamos el dataset en secciones para tener mas datos.
     sampling_rate = 128     # En Hz
     segment_length = 150    # Ventana que se usa en el welch
-    n_components = 5        # Cantidad de components para PCA
+    n_components = 3        # Cantidad de components para PCA
     test_size = 0.2         # Relacion train-test para la red
-    batch_size = 50         # Cantidad de datapoints para cada epoca
-    epochs = 100            # Cantidad de epocas de training para la red
+    batch_size = 35         # Cantidad de datapoints para cada epoca
+    epochs = 60             # Cantidad de epocas de training para la red
 
     # Obtenemos el dataset y lo procesamos (aplicando FFT)
     processed_dataset, dataset_classes = load_and_process_data(sampling_rate, segment_length, clip_length)
@@ -24,8 +25,13 @@ if __name__ == '__main__':
     transformed, _ = apply_pca(matrices, n_components)
 
     # Entrenamos la red
-    X_train, X_test, y_train, y_test = train_test_split(transformed, generate_classes(transformed), test_size=test_size)
+    X_train, X_test, y_train, y_test = train_test_split(transformed, dataset_classes, test_size=test_size)
     model = train_model(X_train, y_train, batch_size, epochs)
+
+    # Entrenamos la red recurrente (que no funciona muy bien)
+    # X, y = sequentialize_data(transformed, dataset_classes)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    # model = train_model_rnn(X_train, y_train, epochs)
 
     # Testeamos la red
     test_loss, test_acc = test_model(X_test, y_test, model)
--- src/neural_network.py
+++ src/neural_network.py
@@ -1,5 +1,9 @@
 import numpy as np
 from keras import Sequential, layers
+from keras.src.layers import SimpleRNN, Dense
+from sklearn.preprocessing import OneHotEncoder
+
+NUM_CLASSES = 4
 
 
 def convert_to_array(dataset: [np.array]):
@@ -7,7 +11,7 @@ def convert_to_array(dataset: [np.array]):
 
 
 def generate_classes(dataset: []):
-    return [i % 4 for i in range(len(dataset))]
+    return [i % NUM_CLASSES for i in range(len(dataset))]
 
 
 def train_model(X_train, y_train, batch_size: int, epochs: int):
@@ -19,10 +23,10 @@ def train_model(X_train, y_train, batch_size: int, epochs: int):
         layers.Input(shape=X[0].shape),
         layers.Dense(64, activation='relu'),
         layers.Dense(32, activation='relu'),
-        layers.Dense(32, activation='relu'),
-        layers.Dense(4, activation='softmax')
+        layers.Dense(16, activation='relu'),
+        layers.Dense(8, activation='relu'),
+        layers.Dense(NUM_CLASSES, activation='softmax')
     ])
-
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy']
@@ -38,5 +42,47 @@ def test_model(X_test, y_test, model):
     y = np.array(y_test).astype("float32")
 
     return model.evaluate(X, y)
+
+
+def get_sequence_length(y_train):
+    prev = y_train[0]
+    for i in range(len(y_train)):
+        if prev != y_train[i]:
+            return i
+    return -1
+
+
+def sequentialize_data(X_data, y_data):
+
+    seq_length = get_sequence_length(y_data)
+    X = []
+
+    for i in range(int(len(X_data) / seq_length)):
+        if i == int(len(X_data) / seq_length):
+            X.append([x.flatten() for x in X_data[i * seq_length:]])    # O(n!), buenisimo
+        else:
+            X.append([x.flatten() for x in X_data[i * seq_length:(i+1) * seq_length]])    # O(n!), buenisimo
+
+    return np.array(X), np.array(generate_classes(X))
+
+
+def train_model_rnn(X_train, y_train, epochs):
+
+    model = Sequential([
+        layers.Input(shape=X_train[0].shape),
+        layers.SimpleRNN(128, activation='relu'),
+        layers.Dense(NUM_CLASSES, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy']
+  )
+
+    model.fit(X_train, y_train, epochs=epochs)
+
+    return model
+
+
 
 
